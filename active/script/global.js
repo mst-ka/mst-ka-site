0f118a65a9c3a216ed2bd888d0a9143744a6772a
Noah <nekgr8@mst.edu>
2020-08-01 20:54:25 -0500

    added global.js file and changed index.html to replace footer phone #'s

---
+++ active/script/global.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const presidentPhone = document.getElementById('presPhone').innerHTML = "(417) 699-6400";
+const alumniPhone = document.getElementById('aluPhone').innerHTML = "(314) 378-8483";
