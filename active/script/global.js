5d38d65329406bbb028ecfe09e15be70b44132a5
Noah <nekgr8@mst.edu>
2020-09-06 21:22:57 -0500

    wowee I used grep and sed to change all the files I am goated

--- active/script/global.js
+++ active/script/global.js
@@ -1,4 +1,4 @@
 'use strict';
 
 const presidentPhone = document.getElementById('presPhone').innerHTML = "(816) 863-4066";
-const alumniPhone = document.getElementById('aluPhone').innerHTML = "(314) 378-8483";
+const alumniPhone = document.getElementById('aluPhone').innerHTML = "Jim Hennessey - (314) 378-8483";
