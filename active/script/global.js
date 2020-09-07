f36cd05ae7289d6aa6853d976923f21556f11e25
Noah <nekgr8@mst.edu>
2020-09-06 21:28:19 -0500

    wip

--- active/script/global.js
+++ active/script/global.js
@@ -1,4 +1,4 @@
 'use strict';
 
-const presidentPhone = document.getElementById('presPhone').innerHTML = "(816) 863-4066";
-const alumniPhone = document.getElementById('aluPhone').innerHTML = "Jim Hennessey - (314) 378-8483";
+document.getElementById('presPhone').innerHTML = "(816) 863-4066";
+document.getElementById('aluPhone').innerHTML = "Jim Hennessey - (314) 378-8483";
