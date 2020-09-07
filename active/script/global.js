aa8f62dd6e7dc68e481e33e3562cc73d0f51fe0a
Noah <nekgr8@mst.edu>
2020-09-06 22:17:20 -0500

    improved script to search for all instances of presPhone and aluPhone

--- active/script/global.js
+++ active/script/global.js
@@ -1,4 +1,10 @@
 'use strict';
 
-document.getElementById('presPhone').innerHTML = "(816) 863-4066";
-document.getElementById('aluPhone').innerHTML = "Jim Hennessey - (314) 378-8483";
+console.log("here");
+
+
+var all_presPhone = document.getElementsByClassName('presPhone');
+var all_aluPhone = document.getElementsByClassName('aluPhone');
+
+[].forEach.call(all_presPhone, function (item) {item.innerHTML = "(816) 863-4066"});
+[].forEach.call(all_aluPhone, function (item) {item.innerHTML = "Jim Hennessey - (314) 378-8483"})
