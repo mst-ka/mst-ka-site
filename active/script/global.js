17e2485ba4964be4dde43c6ebc98f0df22544550
Noah <nekgr8@mst.edu>
2020-09-06 21:19:04 -0500

    removed TEXT from all of the files

--- active/script/global.js
+++ active/script/global.js
@@ -1,4 +1,4 @@
 'use strict';
 
-const presidentPhone = document.getElementById('presPhone').innerHTML = "(417) 699-6400";
+const presidentPhone = document.getElementById('presPhone').innerHTML = "(816) 863-4066";
 const alumniPhone = document.getElementById('aluPhone').innerHTML = "(314) 378-8483";
