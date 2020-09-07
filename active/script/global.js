7a070469725fee4497aa7d43a18995f8a28ae5d7
Noah <nekgr8@mst.edu>
2020-09-06 22:34:59 -0500

    in progress trying to complete the script to work on the contact page

--- active/script/global.js
+++ active/script/global.js
@@ -6,5 +6,7 @@ console.log("here");
 var all_presPhone = document.getElementsByClassName('presPhone');
 var all_aluPhone = document.getElementsByClassName('aluPhone');
 
+console.log(all_presPhone.length);
+
 [].forEach.call(all_presPhone, function (item) {item.innerHTML = "(816) 863-4066"});
 [].forEach.call(all_aluPhone, function (item) {item.innerHTML = "Jim Hennessey - (314) 378-8483"})
