c04f323a45f0220ec069dd64e896a2572ca744d2
Joe <joe.studer.18@gmail.com>
2020-09-26 09:23:08 -0500

    Finalizing a first round draft of a mobile navbar -> only on about.html until approved.

--- active/script/nav.js
+++ active/script/nav.js
@@ -1,8 +1,17 @@
+var unfolded_navs_count = 0;
+
 function displaySubNav(t) {
-  var x = document.getElementById(t);
-  if (x.style.display === "block") {
-    x.style.display = "none";
+  navbar = document.getElementById('navbar');
+  subNav = document.getElementById(t);
+  if (subNav.style.display === "block") {
+    unfolded_navs_count -= 1;
+    if (unfolded_navs_count == 0) {
+      navbar.classList.remove("random-class");
+    }
+    subNav.style.display = "none";
   } else {
-    x.style.display = "block";
+    unfolded_navs_count += 1;
+    navbar.classList.add("random-class");
+    subNav.style.display = "block";
   }
 }
