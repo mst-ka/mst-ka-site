ede7220bddcf6c9548b9d3ece5bb13eb84107423
Joe <joe.studer.18@gmail.com>
2021-07-10 13:00:12 -0500

    Adding script & iframe defers as well as reducing resolution of Instafeed images to improve speedups. +8 mobile score, and +9 desktop score

--- active/script/instafeed-init.js
+++ active/script/instafeed-init.js
@@ -1,7 +1,7 @@
 var feed = new Instafeed({
     accessToken: InstagramToken,
     limit: 5,
-    resolution: 'standard_resolution',
+    resolution: 'low_resolution',
     template: '<a href="{{link}}" target="_blank" id="{{id}}"><img src="{{image}}" /></a>',
     target: 'instafeed'
 });
