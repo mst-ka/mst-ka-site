3e20b2c844db2a0791538261c0f19e00e4714e05
john-goins <97999248+john-goins@users.noreply.github.com>
2022-04-20 21:37:21 -0500

    Jgoins/update officer info (#122)
    * Updated President info
    
    * Updated officer info and images
    
    * Updating pres phone number in global.js
    
    Co-authored-by: jrgdhx <jgoins99@DESKTOP-J91NGF5.localdomain>

--- active/script/global.js
+++ active/script/global.js
@@ -7,5 +7,5 @@ var all_aluPhone = document.getElementsByClassName('aluPhone');
 
 // console.log(all_presPhone.length);
 
-[].forEach.call(all_presPhone, function (item) {item.innerHTML = "(314) 603-5417"});
+[].forEach.call(all_presPhone, function (item) {item.innerHTML = "(417) 824-0849"});
 [].forEach.call(all_aluPhone, function (item) {item.innerHTML = "Jim Hennessey - (314) 378-8483"})
