8484bf4f714ec905569f13154173d8ee91fc7c6e
Jared Hanisch <jared.hanisch@gmail.com>
2022-06-14 21:24:05 -0500

    Fix messed up formatting from prev commit

--- functions/index.js
+++ functions/index.js
@@ -56,58 +56,24 @@ exports.onDataAddedApps = functions.database
     const createdData = snap.val();
     var text = createdData;
     text.subject = "BAofKA Membership Application";
-    text.message =
-      `Name: ${text.firstName} ${text.lastName}` +
-      "<br/><br/>" +
-      "Phone: " +
-      text.phone +
-      "<br/><br/>" +
-      "Email: " +
-      text.email +
-      "<br/><br/>" +
-      `Address: ${text.address} ${text.city}, ${text.state} ${text.zip}` +
-      "<br/><br/>" +
-      "Age: " +
-      text.age +
-      "<br/><br/>" +
-      "High School: " +
-      text.highSchool +
-      "<br/><br/>" +
-      "Class Rank: " +
-      text.classRank +
-      "<br/><br/>" +
-      "GPA: " +
-      text.gpa +
-      "<br/><br/>" +
-      "ACT/SAT: " +
-      text.actSAT +
-      "<br/><br/>" +
-      "Religion: " +
-      text.religion +
-      "<br/><br/>" +
-      "Intended Major: " +
-      text.intendedMajor +
-      "<br/><br/>" +
-      "High School Activities: " +
-      text.highSchoolActivities +
-      "<br/><br/>" +
-      "Honors & Awards: " +
-      text.honorsAwards +
-      "<br/><br/>" +
-      "What are your goals for your first year of college?: " +
-      text.goals +
-      "<br/><br/>" +
-      "Who have you been in contact with at KA?: " +
-      text.contactWith +
-      "<br/><br/>" +
-      "Why are you considering KA?: " +
-      text.whyConsidering +
-      "<br/><br/>" +
-      "What do you like about our Chapter?: " +
-      text.likeAboutChapter +
-      "<br/><br/>" +
-      "How would you define a gentleman?: " +
-      text.gentleman;
+    text.message = `Name: ${text.firstName} ${text.lastName}` + "<br/><br/>" +
+                   "Phone: " + text.phone + "<br/><br/>" +
+                   "Email: " + text.email + "<br/><br/>" +
+                   `Address: ${text.address} ${text.city}, ${text.state} ${text.zip}` + "<br/><br/>" +
+                   "Age: " + text.age + "<br/><br/>" +
+                   "High School: " + text.highSchool + "<br/><br/>" +
+                   "Class Rank: " + text.classRank + "<br/><br/>" +
+                   "GPA: " + text.gpa + "<br/><br/>" +
+                   "ACT/SAT: " + text.actSAT+ "<br/><br/>" +
+                   "Religion: " + text.religion + "<br/><br/>" +
+                   "Intended Major: " + text.intendedMajor + "<br/><br/>" +
+                   "High School Activities: " + text.highSchoolActivities + "<br/><br/>" +
+                   "Honors & Awards: " + text.honorsAwards + "<br/><br/>" +
+                   "What are your goals for your first year of college?: " + text.goals + "<br/><br/>" +
+                   "Who have you been in contact with at KA?: " + text.contactWith + "<br/><br/>" +
+                   "Why are you considering KA?: " + text.whyConsidering + "<br/><br/>" +
+                   "What do you like about our Chapter?: " + text.likeAboutChapter + "<br/><br/>" +
+                   "How would you define a gentleman?: " + text.gentleman;
 
     console.log(`Sending application for ${text.firstName} ${text.lastName}`);
     // Send email with data
