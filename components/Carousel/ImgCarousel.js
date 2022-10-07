e84c995d2a8d2eaf06643a54c7fbf40a02beb9fa
Jared Hanisch <jared.hanisch@gmail.com>
2022-10-06 21:43:01 -0500

    Complete Carousel

--- components/Carousel/ImgCarousel.js
+++ components/Carousel/ImgCarousel.js
@@ -1,63 +1,120 @@
-import { Card, Box, Paper, CardMedia, Typography } from "@mui/material";
+import { Box, CardMedia, Grid, Typography, Button } from "@mui/material";
 import Carousel from "react-material-ui-carousel";
+import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
 function ImgCarousel() {
   var imgs = [
     {
-      heading: "Kappa Alpha Order",
-      sub_heading: "Beta Alpha Chapter",
-      alt_text: "Banner Slide 2",
       src: "images/home/banner-slide-1.jpg",
       button_text: "About Us",
       button_link: "/about",
     },
     {
-      heading: "Kappa Alpha Order",
-      sub_heading: "Beta Alpha Chapter",
-      alt_text: "Banner Slide 2",
       src: "images/home/banner-slide-2.jpg",
-      button_text: "Our Brotherhood",
+      button_text: "Academics Matter",
       button_link: "/awards/scholarships",
     },
     {
-      heading: "Kappa Alpha Order",
-      sub_heading: "Beta Alpha Chapter",
-      alt_text: "Banner Slide 3",
       src: "images/home/banner-slide-3.jpg",
-      button_text: "Academics Matter",
+      button_text: "Our Brotherhood",
       button_link: "/values/bond-of-brotherhood",
     },
   ];
 
   return (
     <Carousel
-      indicators={false}
-      duration={3000}
+      duration={2000}
       interval={6000}
       animation={"fade"}
     >
       {imgs.map((img, index) => (
-        <_CreateImage key={index} image={img} />
+        <CreateImage key={index} image={img} />
       ))}
     </Carousel>
   );
 }
 
-function _CreateImage(props) {
+function CreateImage(props) {
   return (
-    <Box>
-      <CardMedia
-        image={props.image.src}
+    <>
+      <Box>
+        <CardMedia
+          image={props.image.src}
+          sx={{
+            position: "relative",
+            boxShadow: "0rem 1rem 2rem",
+            filter: "brightness(40%)",
+            width: "100%",
+            height: "25rem",
+          }}
+        />
+
+        <Typography
+          color="white.main"
+          variant="h3"
+          sx={{
+            position: "absolute",
+            left: "0",
+            top: { mobile: "10%", tablet: "30%" },
+            textAlign: "center",
+            width: "100%",
+            fontWeight: "500",
+            textShadow: ".1rem .1rem 1rem #000000",
+          }}
+        >
+          Kappa Alpha Order
+        </Typography>
+        <Typography
+          color="white.main"
+          variant="h4"
+          sx={{
+            position: "absolute",
+            left: "0",
+            top: { mobile: "40%", tablet: "45%" },
+            textAlign: "center",
+            width: "100%",
+            fontWeight: "500",
+            textShadow: ".1rem .1rem 1rem #000000",
+          }}
+        >
+          Beta Alpha Chapter
+        </Typography>
+      </Box>
+      <Grid
+        container
+        spacing={2}
         sx={{
-          boxShadow: "0rem 1rem 2rem",
-          filter: "brightness(50%)",
-          width: "100%",
-          height: "25rem",
+          position: "absolute",
+          left: "0",
+          top: "60%",
+          textAlign: "center",
         }}
-      />
-      
-      <Typography color="white.main" variant="h3" sx={{position:"absolute", left: "0", top:"30%", textAlign: "center", width: "100%"}}>This is a test2</Typography>
-    </Box>
+      >
+        <Grid item mobile={12} tablet={6}>
+          <Button
+            color="primary"
+            variant="contained"
+            size="large"
+            href={props.image.button_link}
+            endIcon={<ArrowRightIcon />}
+            sx={{ float: { tablet: "right" } }}
+          >
+            {props.image.button_text}
+          </Button>
+        </Grid>
+        <Grid item mobile={12} tablet={6}>
+          <Button
+            color="primary"
+            variant="contained"
+            size="large"
+            endIcon={<ArrowRightIcon />}
+            sx={{ float: { tablet: "left" } }}
+          >
+            Membership
+          </Button>
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
