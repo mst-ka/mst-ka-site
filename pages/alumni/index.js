c9235fdf340989ff2587ee12f92a1c483c1b9a0d
jaredwhanisch <jared.hanisch@gmail.com>
2023-03-06 23:31:30 -0600

    Add vol 6 of newsletter

--- pages/alumni/index.js
+++ pages/alumni/index.js
@@ -5,6 +5,14 @@ import NewsletterSignup from "../../components/Subpages/Alumni/NewsletterSignUp"
 
 const newsletters = [
   {
+    volumeName: "Vol. 6",
+    link: "https://issuu.com/beta-alpha-alumni-association/docs/ss23_vol6",
+    imageSrc: "/images/alumni/s23_cover.jpeg",
+    altText: "Spring 2023 Newsletter",
+    volumeSubText:
+      "Spring 2023 Volume of the Beta Alpha Alumni Association Journal",
+  },
+  {
     volumeName: "Vol. 5",
     link: "https://issuu.com/beta-alpha-alumni-association/docs/fs22_vol5",
     imageSrc: "/images/alumni/f22_cover.jpeg",
@@ -69,7 +77,7 @@ function Alumni() {
           issue of The BAAA Journal below!
         </Typography>
 
-        <Grid container spacing={6} sx={{marginTop: "0rem"}}>
+        <Grid container spacing={6} sx={{ marginTop: "0rem" }}>
           {newsletters.map((newsletter, index) => (
             <Grid key={index} item mobile={12} tablet={6}>
               <Newsletter
@@ -83,7 +91,7 @@ function Alumni() {
           ))}
         </Grid>
       </Container>
-      <NewsletterSignup/>
+      <NewsletterSignup />
     </div>
   );
 }
