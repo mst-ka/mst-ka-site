2dc8df259316b01726076ea32aefa79748ee06a6
Jared Hanisch <66096385+jaredwhanisch@users.noreply.github.com>
2025-07-28 21:00:24 -0500

    July 28, 2025 Donation Totals (#257)

--- pages/furniture-fundraiser.js
+++ pages/furniture-fundraiser.js
@@ -51,7 +51,7 @@ const FurnitureFundraiser = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
-  const currentAmount = 8594;
+  const currentAmount = 10288;
   const goalAmount = 17000;
   const progress = (currentAmount / goalAmount) * 100;
 
